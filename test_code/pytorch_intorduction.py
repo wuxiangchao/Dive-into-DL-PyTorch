f02db5419fe585933aee37bffc86d8364957df81
WUXIANGCHAO <709894549@qq.com>
2021-03-04 15:15:27 +0800

    Add Test Function

--- test_code/pytorch_intorduction.py
+++ test_code/pytorch_intorduction.py
@@ -2,4 +2,16 @@ from matplotlib import pyplot as plt
 
 class Solution(object):
     def __init__(self):
-        
+        print("Test")
+    
+    def Test(self):
+        try:
+            print("This is Test")
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    s.Test()
